3163c0745e1ecabc3a57a13a8ad7b9d77a5b30ee
Teruki Iida <99105005+handcreamde@users.noreply.github.com>
2022-12-24 18:15:44 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<title><?php echo $_SERVER['HTTP_HOST']; ?>は終了いたしました</title>
+<h1>申し訳ございません。</h1>
+<hr>
+<p><?php echo $_SERVER['HTTP_HOST']; ?> は終了いたしました。<br>ご利用ありがとうございました。</p>
+<p><a href="https://about.2pdesu.ga/">製作者について</a><br>
+<a href="https://labo.wovs.tk/@a">連絡先 (Fediverse)</a></p>
